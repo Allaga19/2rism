1cd2dc963442b55a2ec90c60d7f9d38eae626b58
Алла Гаврюшина <Allaga59yandex.ru>
2023-03-31 08:27:49 +0300

    Закончена вёрстка footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 customSelect('select');
 
-//  Подключение стлей для календаря
+//  Подключение стилей для календаря
 const picker = new easepick.create({
 	lang: 'ru-Ru',
 	element: document.getElementById('date'),
@@ -8,4 +8,17 @@ const picker = new easepick.create({
 		'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
 	],
 	format: 'DD.MM.YY',
+});
+
+// input subscribe__label
+const inputSubscribe = document.querySelector('.subscribe__input');
+const label  = document.querySelector('.subscribe__label ');
+
+inputSubscribe.addEventListener('input', () => {
+	// console.log(1);
+	if (inputSubscribe.value.trim() !== '') {
+		label.classList.add('subscribe__label--top');
+	} else {
+		label.classList.remove('subscribe__label--top');
+	}
 });
